1bd7685ce4f7cd656663f9c860df7c5755426f91
naa-bersa <app20006@byui.edu>
2024-12-10 15:28:27 -0700

    edit inventory not working

--- controllers/invController.js
+++ controllers/invController.js
@@ -192,7 +192,7 @@ invCont.editInventoryView = async function (req, res, next) {
     inv_price: itemData[0].inv_price,
     inv_miles: itemData[0].inv_miles,
     inv_color: itemData[0].inv_color,
-    classification_id: itemData[0]
+    classification_id: itemData[0].classification_id,
 
     
   });
@@ -203,47 +203,64 @@ invCont.editInventoryView = async function (req, res, next) {
  *  Update Inventory Data
  * ************************** */
 invCont.updateInventory = async function (req, res, next) {
-  try {
-    const {
-      inv_id,
-      classification_id,
-      inv_make,
-      inv_model,
-      inv_year,
-      inv_description,
-      inv_image,
-      inv_thumbnail,
-      inv_price,
-      inv_miles,
-      inv_color,
-    } = req.body;
-  // const classificationList = await utilities.buildClassificationList(classification_id);
-
-  // Call the model function to update the database
-  const updateResult = await invModel.updateInventory({
+  let nav = await utilities.getNav()
+  console.log(req.body)
+  const {
     inv_id,
-    classification_id,
     inv_make,
     inv_model,
-    inv_year,
     inv_description,
     inv_image,
     inv_thumbnail,
     inv_price,
+    inv_year,
     inv_miles,
     inv_color,
-  });
+    classification_id,
+  } = req.body
+  // const classificationList = await utilities.buildClassificationList(classification_id);
+  const updateResult = await invModel.updateInventory(
+    inv_id,  
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color,
+    classification_id
+  )
   
   if (updateResult) {
-    req.flash("notice", "Vehicle updated successfully.");
-    res.redirect("/inv/"); // Redirect to the inventory management page
+    const itemName = updateResult.inv_make + " " + updateResult.inv_model
+    req.flash("notice", `The ${itemName} was successfully updated.`)
+    res.redirect("/inv/")
   } else {
-    req.flash("notice", "Vehicle update failed.");
-    res.status(500).redirect(`/inventory/edit-inventory/`);
-  }
-} catch (error) {
-  next(error);
-}
-};
+    console.log("class_id", classification_id)
+    const classificationList = await utilities.buildClassificationList(classification_id)
+    console.log("clasification List:", classificationList);
+    const itemName = `${itemData[0].inv_make} ${itemData[0].inv_model}`
+    req.flash("notice", "Sorry, the insert failed.")
+    res.status(501).render("inventory/edit-inventory", {
+    title: "Edit " ,
+    nav,
+    classificationList,
+    errors: null,
+    flashMessage: null,
+    inv_id: itemData[0].inv_id,
+    inv_make: itemData[0].inv_make,
+    inv_model: itemData[0].inv_model,
+    inv_year: itemData[0].inv_year,
+    inv_description: itemData[0].inv_description,
+    inv_image: itemData[0].inv_image,
+    inv_thumbnail: itemData[0].inv_thumbnail,
+    inv_price: itemData[0].inv_price,
+    inv_miles: itemData[0].inv_miles,
+    inv_color: itemData[0].inv_color,
+    classification_id: itemData[0].classification_id,
+  });
+};}
   
 module.exports = invCont;
